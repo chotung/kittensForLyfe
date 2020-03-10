86fc531f8043fad3a6c769ca9bcf254fbfd33ff9
chotung <choelestung@gmail.com>
2020-03-09 23:43:23 -0400

    got data from backend to frontend and used hooks to render data

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,86 @@
-import React from "react";
+// import React from "react";
 import axios from "axios"
 
 
-class App extends React.Component {
+// class App extends React.Component {
 
-  state = {
-    kittens: []
-  }
-  componentDidMount() {
-    this.getAllKittens()
-  }
+//   state = {
+//     kittens: []
+//   }
+//   componentDidMount() {
+//     this.getAllKittens()
+//   }
 
-  getAllKittens = async () => {
+//   getAllKittens = async () => {
+//     const response = await axios("/api/kittens")
+//     const { data } = await response
+//     this.setState({
+//       kittens:data
+//     })
+//   }
+  
+//   showMeAllKittens = () => {
+//     return  this.state.kittens.map((kitten, index) => {
+//       return (
+//         <li key={kitten._id}id={index}>{kitten.name}</li>
+//       )
+//     })
+//   }
+  
+//   render() {
+//     console.log(this.state)
+//     return (
+//       <div className="App">
+//         <ul className="kittens">
+//         {this.showMeAllKittens()}
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
+//=========================================================
+
+import React, { useState, useEffect} from "react";
+
+const App = () => {
+
+  const [kittens, setKittens] = useState()
+
+  useEffect(() => {
+    getAllKittens()
+  }, [])
+  
+  const getAllKittens = async () => {
     const response = await axios("/api/kittens")
     const { data } = await response
-    this.setState({
-      kittens:data
+    setKittens({
+      kittens: data
     })
   }
+
   
-  
-  showMeAllKittens = () => {
-    return  this.state.kittens.map((kitten, index) => {
+  const showMeAllKittens = () => {
+    // WE're not getting kittens
+    return kittens.kittens.map((kitten, index) => {
       return (
         <li key={kitten._id}id={index}>{kitten.name}</li>
       )
     })
   }
   
-
-
-  render() {
-    console.log(this.state)
-    return (
-      <div className="App">
-        <ul className="kittens">
-        {this.showMeAllKittens()}
-        </ul>
-      </div>
-    )
-  }
+  
+  return (
+    <div>
+      <ul className="kittens">
+        {kittens === undefined ? "Loading..." : showMeAllKittens()}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
+
+// context + useReducer 
+// WRAP WHOLE APP
